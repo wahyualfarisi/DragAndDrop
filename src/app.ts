508702facfc1869138d16e4fa9fa05adee07f0c9
wahyu alfarisi <wahyualfarisi@gmail.com>
2022-11-26 19:03:09 +0700

    Create Project item class and set getter

--- src/app.ts
+++ src/app.ts
@@ -137,6 +137,34 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+//Project Item class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get peopleAssign(){
+    if (this.project.people === 1) return "1 Person"
+    return this.project.description + " Person";
+  }
+
+  constructor(hostId: string, project: Project){
+    super('single-project', hostId, false, project.id)
+    this.project = project;
+
+    this.configure()
+    this.renderContent()
+  }
+
+  configure(): void {
+    
+  } 
+
+  renderContent(): void {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.peopleAssign + " Assigned."
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 //ProjectList class
 type ProjectType = 'active' | 'finished';
 
@@ -168,9 +196,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = ''
     for (const prjItem of this.assignedProjects){
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl?.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem as Project)
     }
   }
 }
